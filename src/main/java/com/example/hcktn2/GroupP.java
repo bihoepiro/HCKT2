ec5c9db58aa71e3ea46725290e89b0b51664f840
bihoepiro <83984593+bihoepiro@users.noreply.github.com>
2023-11-06 16:53:19 -0500

    xd

---
+++ src/main/java/com/example/hcktn2/GroupP.java
@@ -0,0 +1,55 @@
+package com.example.hcktn2;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "GroupP")
+public class GroupP {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "group_persons",
+            joinColumns = { @JoinColumn(name = "id_group") },
+            inverseJoinColumns = { @JoinColumn(name = "id_person") }
+    )
+    private Set<Person> members = new HashSet<>();
+
+    public GroupP() {
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Person> getMembers() {
+        return members;
+    }
+    public void setMembers(Set<Person> members) {
+        this.members = members;
+    }
+
+    public GroupP(String name, Long id, Set<Person> members) {
+        this.name = name;
+        this.id = id;
+        this.members = members;
+    }
+}
---
+++ src/main/java/com/example/hcktn2/Hcktn2Application.java
@@ -0,0 +1,13 @@
+package com.example.hcktn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hcktn2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hcktn2Application.class, args);
+    }
+
+}
